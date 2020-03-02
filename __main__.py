7465e0df971e54c63be41bfb5f0587325379afb7
Rod Spencer <github@rodspencer.net>
2020-03-02 12:34:14 -0600

    arrow keys + control & arrow keys

--- __main__.py
+++ __main__.py
@@ -1,39 +1,63 @@
 #!/usr/bin/env python3
 
+# todo & notes
+# shift functionality does not work with arrow keys - maybe with pg u/d / home/end?
+# implement all arrow and ctrl+arrow keys
+# implement home/end & pg up/pg dwn
+# impelment backspace delete
+
 # pip install keyboard
 import keyboard
 
-# block capslock key from changing capslock state:
-keyboard.add_hotkey('capslock', lambda: print('should not enable capslock'), suppress=True)
+# BINDS: 
 
-# map caps+a to capslock:
+# block capslock key from changing capslock state:
+keyboard.add_hotkey('capslock', lambda: empty(), suppress=True)
+
+# MAP CAPSLOCK:
 keyboard.add_hotkey('capslock+a', lambda: keyboard.send('capslock'), suppress=True)
 
-# map caps+j to left arrow:
+# MAP ARROW KEYS
+# arrow keys:
 keyboard.add_hotkey('capslock+j', lambda: keyboard.send('left'), suppress=True)
+keyboard.add_hotkey('capslock+k', lambda: keyboard.send('up'), suppress=True)
+keyboard.add_hotkey('capslock+l', lambda: keyboard.send('down'), suppress=True)
+keyboard.add_hotkey('capslock+;', lambda: keyboard.send('right'), suppress=True)
+# ctrl+arrow keys:
 keyboard.add_hotkey('capslock+ctrl+j', lambda: keyboard.send('ctrl+left'), suppress=True)
-keyboard.add_hotkey('capslock+ctrl+shift+j', lambda: hold_shift(), suppress=True)
-# to implement ctrl+shift+left: create hook(?) 
-# while keys are pressed, shift key_down
-# if j then send left
-# if keys are not pressed, shift key_up
-# # keyboard.add_hotkey('capslock+ctrl+shift+j', lambda: keyboard.send('ctrl+shift+left'), suppress=True)
+keyboard.add_hotkey('capslock+ctrl+k', lambda: keyboard.send('ctrl+up'), suppress=True)
+keyboard.add_hotkey('capslock+ctrl+l', lambda: keyboard.send('ctrl+down'), suppress=True)
+keyboard.add_hotkey('capslock+ctrl+;', lambda: keyboard.send('ctrl+right'), suppress=True)
 
-print(__name__)
+# MAP PAGE UP/DOWN
+# pg up / pg dn:
+# keyboard.add_hotkey('capslock+,', lambda: keyboard.send('page up'), suppress=True)
+# keyboard.add_hotkey('capslock+.', lambda: keyboard.send('page down'), suppress=True)
+# ctrl+ keys:
+# keyboard.add_hotkey('capslock+ctrl+j', lambda: keyboard.send('ctrl+left'), suppress=True)
+# keyboard.add_hotkey('capslock+ctrl+k', lambda: keyboard.send('ctrl+up'), suppress=True)
 
-def hold_shift():
-    while(keyboard.is_pressed('shift')):
-        keyboard.press('shift')
-        if keyboard.is_pressed('j'):
-            keyboard.send('ctrl+left')
-        keyboard.release('shift')
+# SHIFT FUNCTIONALITY - On hold due to bug (Issue #330 on GitHub)
+# keyboard.add_hotkey('capslock+ctrl+shift+j', lambda: keyboard.send('ctrl+shift+left'), suppress=True)
+# keyboard.add_hotkey('capslock+ctrl+shift+;', lambda: keyboard.send('ctrl+shift+right'), suppress=True)
+# keyboard.add_hotkey('ctrl+shift+x', lambda: keyboard.send('shift+left'), suppress=True)
+# keyboard.add_hotkey('ctrl+shift+x', lambda: hold_shift(), suppress=True)
+#
+# def hold_shift():
+#     keyboard.press('left')
+#     keyboard.release('left')
+
+
+# FUNCTIONS: 
+
+# empty function used in blocking capslock key from changing capslock state
+def empty():
+    pass
 
 def main():
     flag = 1
     while(flag): 
-        print(flag)
+        pass
 
 if __name__ == '__main__':
     main()
-
-
