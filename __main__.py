3d6ea18612f2c3b9fdd7f1932115d6aa74de8216
Rod Spencer <github@rodspencer.net>
2020-03-02 12:45:33 -0600

    page home end delete added

--- __main__.py
+++ __main__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 # todo & notes
-# shift functionality does not work with arrow keys - maybe with pg u/d / home/end?
-# implement all arrow and ctrl+arrow keys
-# implement home/end & pg up/pg dwn
+# shift functionality does not currently work correctly
+# implement home/end
 # impelment backspace delete
 
 # pip install keyboard
@@ -14,28 +13,35 @@ import keyboard
 # block capslock key from changing capslock state:
 keyboard.add_hotkey('capslock', lambda: empty(), suppress=True)
 
-# MAP CAPSLOCK:
+# CAPSLOCK:
 keyboard.add_hotkey('capslock+a', lambda: keyboard.send('capslock'), suppress=True)
 
-# MAP ARROW KEYS
-# arrow keys:
+# ARROW KEYS
 keyboard.add_hotkey('capslock+j', lambda: keyboard.send('left'), suppress=True)
 keyboard.add_hotkey('capslock+k', lambda: keyboard.send('up'), suppress=True)
 keyboard.add_hotkey('capslock+l', lambda: keyboard.send('down'), suppress=True)
 keyboard.add_hotkey('capslock+;', lambda: keyboard.send('right'), suppress=True)
-# ctrl+arrow keys:
+# ctrl+:
 keyboard.add_hotkey('capslock+ctrl+j', lambda: keyboard.send('ctrl+left'), suppress=True)
 keyboard.add_hotkey('capslock+ctrl+k', lambda: keyboard.send('ctrl+up'), suppress=True)
 keyboard.add_hotkey('capslock+ctrl+l', lambda: keyboard.send('ctrl+down'), suppress=True)
 keyboard.add_hotkey('capslock+ctrl+;', lambda: keyboard.send('ctrl+right'), suppress=True)
 
-# MAP PAGE UP/DOWN
-# pg up / pg dn:
-# keyboard.add_hotkey('capslock+,', lambda: keyboard.send('page up'), suppress=True)
-# keyboard.add_hotkey('capslock+.', lambda: keyboard.send('page down'), suppress=True)
-# ctrl+ keys:
-# keyboard.add_hotkey('capslock+ctrl+j', lambda: keyboard.send('ctrl+left'), suppress=True)
-# keyboard.add_hotkey('capslock+ctrl+k', lambda: keyboard.send('ctrl+up'), suppress=True)
+# PAGE UP/DOWN
+keyboard.add_hotkey('capslock+comma', lambda: keyboard.send('page up'), suppress=True)
+keyboard.add_hotkey('capslock+.', lambda: keyboard.send('page down'), suppress=True)
+
+# HOME/END KEYS
+keyboard.add_hotkey('capslock+i', lambda: keyboard.send('home'), suppress=True)
+keyboard.add_hotkey('capslock+o', lambda: keyboard.send('end'), suppress=True)
+# ctrl+:
+keyboard.add_hotkey('capslock+ctrl+i', lambda: keyboard.send('ctrl+home'), suppress=True)
+keyboard.add_hotkey('capslock+ctrl+o', lambda: keyboard.send('ctrl+end'), suppress=True)
+
+# DELETE
+keyboard.add_hotkey('capslock+backspace', lambda: keyboard.send('delete'), suppress=True)
+# ctrl+:
+keyboard.add_hotkey('capslock+ctrl+backspace', lambda: keyboard.send('ctrl+delete'), suppress=True)
 
 # SHIFT FUNCTIONALITY - On hold due to bug (Issue #330 on GitHub)
 # keyboard.add_hotkey('capslock+ctrl+shift+j', lambda: keyboard.send('ctrl+shift+left'), suppress=True)
@@ -46,7 +52,6 @@ keyboard.add_hotkey('capslock+ctrl+;', lambda: keyboard.send('ctrl+right'), supp
 # def hold_shift():
 #     keyboard.press('left')
 #     keyboard.release('left')
-
 
 # FUNCTIONS: 
 
